b19c92c1e90f1c173cfae6d59eb3a50220abe4ad
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-26 01:32:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,11 @@
 def key_for_min_value(name_hash)
   lowest = {key: nil, value: nil}
   name_hash.each do |key, value|
-     if lowest[:value] == nil
-       lowest[:key] = key.to_s
+    if lowest[:value] == nil
+     lowest[:key] = key
+     lowest[:value] = value
+    elsif lowest[:value] > value
+      
   puts "#{key}: #{value}"
 end
   end
